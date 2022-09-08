b97295709cff058f6ea4305184f9f024ff26b7b3
Zyad <>
2022-09-08 09:52:24 +0100

    Mergesort working with 4 digit array only

--- mergesort.rb
+++ mergesort.rb
@@ -1,26 +1,65 @@
 # require 'pry-byebug'
 
 
-@sortingArray = []
+
+
+
+@sortingArray1 = []
+@sortingArray2 = []
 def merge_sort(array)
-  # binding.pry
+    # binding.pry
+
   if array.length < 2
     return
   else
     merge_sort(arrayLeft = array.slice(0, array.length/2))
     merge_sort(arrayRight = array.slice((array.length/2)..))
+    # @sortingArray1 << merge(arrayLeft, arrayRight)
+    # p @sortingArray1
   end
 
   # merging steps
-  if arrayLeft[0] < arrayRight[0]
-    @sortingArray << arrayLeft[0]
-    @sortingArray << arrayRight[0]
-  else
-    @sortingArray << arrayRight[0]
-    @sortingArray << arrayLeft[0]
+  if @sortingArray2.length > 1
+
+    while !@sortingArray2[0].empty? || !@sortingArray2[1].empty?
+      begin
+        if @sortingArray2[0][0] <= @sortingArray2[1][0]
+          @sortingArray1 << @sortingArray2[0].shift
+        else
+          @sortingArray1 << @sortingArray2[1].shift
+        end
+      rescue
+        @sortingArray1 = @sortingArray1 + @sortingArray2
+        p @sortingArray1.flatten
+        return
+      end
+    end
+
   end
-  p @sortingArray
-  # arrayLeft = sortingArray
+  if arrayLeft[0] < arrayRight[0]
+    @sortingArray1 << arrayLeft.shift
+    @sortingArray1 << arrayRight.shift
+  else
+    @sortingArray1 << arrayRight.shift
+    @sortingArray1 << arrayLeft.shift
+  end
+  @sortingArray2 << @sortingArray1
+  @sortingArray1 = []
+
 end
 
-merge_sort([7, 3, 2, 1, 4, 8, 5])
+
+merge_sort([7, 3, 2, 1, 6])
+
+# def merge(arr1, arr2)
+#   sortingArray = []
+#   while arr1.empty? == false && arr2.empty? == false
+#     if arr1.first <= arr2.first
+#       sortingArray << arr1.shift
+#     else
+#       sortingArray << arr2.shift
+#     end
+#   end
+#   sortingArray = sortingArray + arr1 + arr2
+#   sortingArray
+# end
